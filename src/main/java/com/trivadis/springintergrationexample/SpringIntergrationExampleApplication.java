dc0b02f1943b42419a36248f253738cbffacf279
mog <Amine.Grioui@trivadis.com>
2021-09-28 10:28:40 +0200

    first commit

---
+++ src/main/java/com/trivadis/springintergrationexample/SpringIntergrationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.trivadis.springintergrationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntergrationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntergrationExampleApplication.class, args);
+	}
+
+}
